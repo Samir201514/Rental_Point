889f83374f6feb823b9c4c300ff4e41fcdc6135c
Samir201514 <arafat.rahman3926@gmail.com>
2026-08-31 02:31:33 +0600

    add JS Folder & Start coding...

---
+++ profile.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title> Profile </title>
+    <link rel="stylesheet" href="CSS/profile.css">
+</head>
+<body>
+    <header></header>
+
+
+    <main>
+        <section id="photo">
+            <div id="cover">
+                <img src="" width="500px">
+            </div>
+
+            <div id="proPic">
+                <div id="picName">
+                    <h2 id="userName"> <i> Name </i> </h2>
+                    <span> ✅ Verified </span>
+                    <br>
+                    <span id="userType"> Type </span>
+                    <span> . </span>
+                    <span id="userLocation"> Location </span>
+                </div>
+            </div>
+            
+            <p id="bio">
+                My Bio
+            </p>
+        </section>
+        
+        <section id="tab">
+            <a href="#about" class="tab"> About </a>
+            <a href="#myPost" class="tab"> My Posts </a>
+            <a href="#savedPost" class="tab"> Saved Posts </a>
+            <a href="#settings" class="tab"> Settings </a>
+            <hr>
+        </section>
+        
+        <section id="about">
+            <h3> Personal Information </h3>
+            <br>
+            
+            <span> Name </span>
+            <span> Samir </span>
+            <hr>
+            <span> Email </span>
+            <span> arafat.rahman3926@gmail.com </span>
+            <hr>
+            <span> Phone </span>
+            <span> 01575431438 </span>
+            <hr>
+            <span> Location </span>
+            <span> Dhaka </span>
+            <hr>
+            <div id="editBtn">
+                <button> Edit Profile Details </button>
+            </div>
+        </section>
+    </main>
+
+
+    <footer></footer>
+</body>
+</html>
