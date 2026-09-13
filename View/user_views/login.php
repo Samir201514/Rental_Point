ce8025820d0df171b62fb37bd0691346da46cdb1
Samir201514 <arafat.rahman3926@gmail.com>
2026-09-13 21:52:20 +0600

    Fixed the controller parte & create account input filleds

--- View/user_views/login.php
+++ View/user_views/login.php
@@ -50,7 +50,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <p>Enter your credentials to access verified listings</p>
       </div>
       
-      <form action="login.php" method="POST">
+      <form action="../../Controller/homeController.php" method="POST">
         <?php if (!empty($errors) && empty($errors['email']) && empty($errors['password'])): ?>
         <div class="alert-error">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"/></svg>
@@ -93,6 +93,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       </div>
     </div>
   </div>
-
 </body>
 </html>
